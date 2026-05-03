f1e4024526f463a1d90f197bbce30171a66b6aa3
Макс <maksim5494@yandex.ru>
2026-05-03 23:07:17 +0500

    1

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * TODO Sprint add-controllers.
  */
+
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
