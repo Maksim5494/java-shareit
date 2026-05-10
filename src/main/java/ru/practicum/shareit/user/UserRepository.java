971b1f69814fcb838ecd5f5adcc9aaa21dd9d2f4
Макс <maksim5494@yandex.ru>
2026-05-10 12:56:35 +0500

    1

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,11 +1,12 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
 
     User update(User user);
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,14 +1,26 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import jakarta.persistence.*;
 
-@Data
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Email
+    @NotBlank
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 }
