00932e76814869cd3b1b117fb6a2f89a0f82fd66
Макс <maksim5494@yandex.ru>
2026-05-10 19:22:32 +0500

    18

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -2,6 +2,8 @@ package ru.practicum.shareit.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,6 +27,20 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        log.error("400 Validation Error: {}", e.getMessage());
+        return new ErrorResponse("Ошибка валидации данных.");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingHeader(final MissingRequestHeaderException e) {
+        log.error("400 Missing Header: {}", e.getMessage());
+        return new ErrorResponse("Отсутствует обязательный заголовок: " + e.getHeaderName());
+    }
+
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConflictException(final ConflictException e) {
         log.error("409: {}", e.getMessage());
@@ -34,7 +50,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
-        log.error("500: {}", e.getMessage(), e);
+        log.error("500: Unexpected error", e);
         return new ErrorResponse("Произошла непредвиденная ошибка.");
     }
 }
