b6e55b6c133aac9a88191d1fcce8e4be1741b09b
Макс <maksim5494@yandex.ru>
2026-05-10 18:20:59 +0500

    14

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,31 +1,34 @@
 package ru.practicum.shareit.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleNotFound(final NotFoundException e) {
-        return Map.of("error", e.getMessage());
+    public ErrorResponse handleNotFound(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidation(final MethodArgumentNotValidException e) {
-        return Map.of("error", "Validation failed");
+    public ErrorResponse handleValidation(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleConflict(final ConflictException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleThrowable(final Throwable e) {
-        return Map.of("error", "Internal server error");
+    public ErrorResponse handleThrowable(final Throwable e) {
+        return new ErrorResponse("Внутренняя ошибка сервера");
     }
 }
