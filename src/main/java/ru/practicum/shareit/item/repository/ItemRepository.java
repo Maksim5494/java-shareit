620110cda3377ea4a8aa4a862d5e229b74db4d12
Макс <maksim5494@yandex.ru>
2026-05-10 13:58:40 +0500

    4

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,18 +1,24 @@
 package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
-@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findAllByOwner_Id(Long ownerId);
 
-    List<Item> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCaseAndAvailableIsTrue(String name, String description);
+    @Query("""
+        select i from Item i
+        where i.available = true
+        and (
+            lower(i.name) like lower(concat('%', :text, '%'))
+            or lower(i.description) like lower(concat('%', :text, '%'))
+        )
+        """)
 
     List<Item> searchAvailableByText(@Param("text") String text);
 
