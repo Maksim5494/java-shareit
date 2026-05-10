2f7e5b91bad50712cd424e3d2ab8172af39531ab
Макс <maksim5494@yandex.ru>
2026-05-10 22:00:17 +0500

    30

--- src/main/java/ru/practicum/shareit/exceptions/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorResponse.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class ErrorResponse {
+
     private final String error;
-    public ErrorResponse(String error) { this.error = error; }
+
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
+
 }
