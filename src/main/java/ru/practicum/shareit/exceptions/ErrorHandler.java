a64fa6eaa76a2434eff0ed6776a111392f9967e9
Макс <maksim5494@yandex.ru>
2026-05-10 18:24:42 +0500

    14,1

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,34 +1,40 @@
 package ru.practicum.shareit.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
-    @ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFound(final NotFoundException e) {
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        log.error("404: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(ValidationException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidation(final ValidationException e) {
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        log.error("400: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(ConflictException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleConflict(final ConflictException e) {
+    public ErrorResponse handleConflictException(final ConflictException e) {
+        log.error("409: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(Throwable.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
-        return new ErrorResponse("Внутренняя ошибка сервера");
+        log.error("500: {}", e.getMessage(), e);
+        return new ErrorResponse("Произошла непредвиденная ошибка.");
     }
 }
--- src/main/java/ru/practicum/shareit/exceptions/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorResponse.java
@@ -9,4 +9,8 @@ public class ErrorResponse {
     public ErrorResponse(String error) {
         this.error = error;
     }
+
+    public String getError() {
+        return error;
+    }
 }
